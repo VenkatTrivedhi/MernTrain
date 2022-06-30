2fe1ab55fa8413d500e90d239e15e6b3d51d2375
VenkatTrivedhi <k.s.venkat614@gmail.com>
2022-06-30 13:27:47 +0530

    more cleaner

--- armstrong.js
+++ armstrong.js
@@ -1,16 +1,28 @@
-for (let i=0;i<=100000;i++){
+//iterating each number from 0 to 100000
+for (let number=0;number<=100000;number++){
 
     let amstrongSum = 0
-    number= ""+i
-    length = number.length
 
-    for(let digit_string of number){  
-        digit_int = digit_string*1  
-        cube = digit_int**length
-        amstrongSum = amstrongSum + cube 
+    //convert number into string form
+    number_string= ""+ number
+
+    //no of digits in number
+    length = number_string.length
+
+    
+    //getting amstrongSum of number, by iterating each digit
+    for(let digit_charecter of number_string){
+        
+        //converting each digit into integer  
+        digit_integer = digit_charecter*1
+    
+        power = digit_integer**length
+        amstrongSum = amstrongSum + power 
     } 
-    if (amstrongSum==i){
-        console.log(i)
+
+    //checking whether number is amstron
+    if (amstrongSum==number){
+        console.log('${i} is Amstrong number')
     }
 
 }
