636a55a23e70e62932b92879f399a69aebc8168a
VenkatTrivedhi <k.s.venkat614@gmail.com>
2022-06-30 13:50:42 +0530

    more cleaner

--- fibonaci.js
+++ fibonaci.js
@@ -1,22 +1,24 @@
 //function sum of fibanoci series
-var terminated = false 
-let fibanoci=(first,second)=>{
-    console.log(first,second)
+let fibanoci=(firstNumber,secondNumber)=>{
    
-    if (second>=5000){
-        return first
-    }   
-    let temp = second
-    second = first+second
-    first = temp
-    return first + fibanoci(first,second)
+    if (secondNumber>=5000){
+        return firstNumber
+    }
+    
+    //creating and shifting to next 2 numbers
+    let temporary = secondNumber
+    secondNumber = firstNumber + secondNumber
+    firstNumber = temporary
+
+    //return sum of all first numbers less 5000 in series 
+    return firstNumber + fibanoci(firstNumber,secondNumber)
 }
 
 //function for reading a valid number type 
-
-let readAndHandle=(msg="enter number")=>{
+let readInput=(msg="enter number")=>{
     let arr =[0,1]
 
+    //changing initial 2 numbers
     for (let i=0;i<2;i++){
         let number = i+1
         let message =  msg + number
@@ -26,21 +28,22 @@ let readAndHandle=(msg="enter number")=>{
             console.log('exited,refresh restart')
             return
         }
+
         //converting input into number type
-        
         input = input*1
         
-
+        //allowing only numbers
         if (!input&&input!=0){
             let message = "enter numbers only,for Number " 
             console.log(message)
-            readAndHandle(message)
+            readInput(message)
         }
+
         //restrict negitive number 
         if (input<0){
             let message = "enter positive numbers only,for Number " 
             console.log(message)
-            readAndHandle(message)
+            readInput(message)
         }
 
         if (input>=0){
@@ -54,4 +57,4 @@ let readAndHandle=(msg="enter number")=>{
     console.log(fibanoci(firstNumber,secondNumber))
 }
     
-readAndHandle()
+readInput()
